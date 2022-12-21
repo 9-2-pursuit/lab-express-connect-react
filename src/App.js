0e263c11c21ff6efb0b38927c1b4fe5f915ba039
Bo Latt <bolattny@gmail.com>
2022-12-20 19:00:28 -0500

    add routes, empty pages , navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Logs from "./pages/Logs";
+import Log from "./pages/Log";
+import NewLog from "./pages/NewLogs";
+import EditLog from "./pages/EditLog";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Navbar />
+          <Route path="/" element={<Home />} />
+          <Route path="/logs" element={<Logs />} />
+          <Route path="/logs/:index" element={<Log />} />
+          <Route path="/logs/new" element={<NewLog />} />
+          <Route path="/logs/:index/edit" element={<EditLog />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
