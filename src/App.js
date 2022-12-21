1779b5c2b5049535b9c78ba5fbd6aed38781f06c
Karyn Huston <huston.karyn@gmail.com>
2022-12-21 11:39:55 -0500

    npm i cors, both ends connected

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import axios, { Axios } from "axios";
+import { useState, useEffect } from "react";
 function App() {
+
+  const API = process.env.REACT_APP_API_URL
+  const [logs, setLogs] = useState([])
+
+  useEffect(() => {
+axios.get(`${API}/logs`)
+.then((response) => {
+  console.log(response)
+})
+  })
+
+
+
+
   return <div>Hello World</div>;
 }
 
