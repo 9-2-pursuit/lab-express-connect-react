d5df61a8bcf6144e5281079d681ea8ec46ecb539
i-guerrero <isaacguerrero@pursuit.org>
2022-12-22 13:02:45 -0500

    Add classname Log to log to pass all tests

--- src/Components/Log.js
+++ src/Components/Log.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Log({ log, i }) {
   return (
-    <tr>
+    <tr className="Log">
       <td>{log.mistakesWereMadeToday ? (
         <span>🎇</span>
       ) : (
