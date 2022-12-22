81d61365288e1a90c0122c5f7bff0c3141f146fa
i-guerrero <isaacguerrero@pursuit.org>
2022-12-22 14:14:55 -0500

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import LogsIndex from "./Components/LogsIndex";
 import LogDetails from "./Components/LogDetails";
 import NewLogForm from "./Components/NewLogForm";
 import EditLogForm from "./Components/EditLogForm";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
     <div>
       <Router>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/logs" element={<LogsIndex />} />
-          <Route path="/logs/:index" element={<LogDetails />} />
-          <Route path="/logs/new" element={<NewLogForm />} />
-          <Route path="/logs/:index/edit" element={<EditLogForm />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<LogsIndex />} />
+            <Route path="/logs/:index" element={<LogDetails />} />
+            <Route path="/logs/new" element={<NewLogForm />} />
+            <Route path="/logs/:index/edit" element={<EditLogForm />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
       </Router>
     </div>
   );
